ce929637fb153348e3c36f7182f6406101918988
Fernando Bartolomé <fbartolome-externo@unir.net>
2023-01-19 09:38:54 +0100

    feat: live search on podcasts list page

--- src/components/InputSearch/InputSearch.js
+++ src/components/InputSearch/InputSearch.js
@@ -1,10 +1,17 @@
-export default function InputSearch() {
+import PropTypes from 'prop-types';
+
+export default function InputSearch({onInput}) {
   return (
     <input
       className="form-control"
       type="search"
       placeholder="Filter podcasts..."
       aria-label="Filter podcasts"
+      onInput={onInput}
     />
   );
 }
+
+InputSearch.propTypes = {
+  onInput: PropTypes.func.isRequired,
+};
